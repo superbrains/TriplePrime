3f8a211e0955ae79f4bfd9ab4d2d968c5585d5c9
obia-optimus <obi.azubike@optimusinfo.com>
2025-08-05 09:30:18 +0100

    Updated

---
+++ src/TriplePrime.Data/Migrations/20250805082145_Token.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TriplePrime.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Token : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceToken",
+                table: "AspNetUsers",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceToken",
+                table: "AspNetUsers");
+        }
+    }
+}
